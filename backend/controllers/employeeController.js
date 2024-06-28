f900a236730cb36ab0a9a8136a185c381408f3dd
Dolfis123 <kdolfis@gmail.com>
2024-06-28 15:02:51 -0700

    selesai-8

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -2,6 +2,7 @@ const db = require("../config/database");
 const moment = require("moment-timezone");
 const path = require("path");
 const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -50,6 +51,7 @@ const createEmployee = (req, res) => {
     } = req.body;
 
     const photo = req.file ? req.file.filename : null;
+    const employee_id = uuidv4(); // Generate UUID for employee_id
 
     if (!full_name || !nip || !join_date || !employment_status) {
       console.error("Validation error: Missing required fields");
@@ -71,10 +73,11 @@ const createEmployee = (req, res) => {
 
       const sqlQuery = `
         INSERT INTO employees
-        (full_name, nip, address, birth_date, phone_number, email, position, join_date, employment_status, department, gender, marital_status, education, blood_type, retirement_date, nationality, photo)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        (employee_id, full_name, nip, address, birth_date, phone_number, email, position, join_date, employment_status, department, gender, marital_status, education, blood_type, retirement_date, nationality, photo)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
       `;
       const values = [
+        employee_id,
         full_name,
         nip,
         address,
