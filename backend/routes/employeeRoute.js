6c6658b03bffabce9af854ac9b66877fb9267b57
Dolfis123 <kdolfis@gmail.com>
2024-06-28 17:41:18 -0700

    selesai-14

--- backend/routes/employeeRoute.js
+++ backend/routes/employeeRoute.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const employeeController = require("../controllers/employeeController");
 
-router.post("/employees", employeeController.createEmployee);
+router.post("/employees-all", employeeController.createEmployee);
 router.get("/employees", employeeController.getAllEmployees);
 router.get("/employees/:employee_id", employeeController.getEmployeeById);
 router.put("/employees/:employee_id", employeeController.updateEmployee);
 router.delete("/employees/:employee_id", employeeController.deleteEmployee);
-router.get("/api/employees", employeeController.getAllEmployees1);
+router.get("/employees", employeeController.getAllEmployees1);
 
 module.exports = router;
