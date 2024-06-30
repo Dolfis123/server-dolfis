ac2ad2df95bd7d95442852faab547ec30e53a3ac
Dolfis123 <kdolfis@gmail.com>
2024-06-30 16:58:15 -0700

    selesai-1

--- backend/routes/beritaRoute.js
+++ backend/routes/beritaRoute.js
@@ -4,7 +4,6 @@ const newsController = require("../controllers/beritaController");
 
 router.get("/news", newsController.getAllNews);
 router.get("/news_status", newsController.getAllNewsPublished); // Rute yang telah diperbaiki
-
 router.get("/news/:id", newsController.getById);
 router.post("/news", newsController.createNews);
 router.put("/news/:id", newsController.updateNews);
---
+++ frontend/src/pages/public/Tst.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tst() {
+  return <div>Tst</div>;
+}
+
+export default Tst;
